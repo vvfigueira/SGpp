f5d0229498c99217e3022adb6818a627db002dd0
vfigueira00 <vfigueira@usp.br>
2022-03-10 19:43:28 -0300

    Adição de arquivo para compilação

---
+++ comp.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+mkdir SGpp_build
+cd SGpp_build
+
+source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_101/x86_64-centos7-gcc11-opt/share/Garfield/setupGarfield.sh
+
+cmake ../source
+make
+
+./build 100 #Número de Elétrons para ser simulado por Core
